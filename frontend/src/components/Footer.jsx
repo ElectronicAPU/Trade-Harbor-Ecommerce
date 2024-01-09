1eb63ae2d77cd77858999dfe3768121c45bf4c95
ElectronicAPU <apurbanaskar3206@gmail.com>
2024-01-09 15:42:04 +0530

    Header and Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const Footer = () => {
+  const currYear = new Date().getFullYear();
+  return (
+    <>
+      <footer>
+        <Container>
+          <Row>
+            <Col className="text-center py-3">
+              <p>TradeHarbou &copy; {currYear}</p>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
