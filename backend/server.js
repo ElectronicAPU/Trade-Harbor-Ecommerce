e0821861c811d3fc36dd186291da8dee9235f71d
ElectronicAPU <apurbanaskar3206@gmail.com>
2024-01-10 12:14:15 +0530

    Install and use dotenv in server.js and also package.json updated

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import products from "./data/products.js";
+import dotenv from "dotenv";
 
-const PORT = 5000;
+dotenv.config();
+
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.get("/", (req, res) => {
@@ -11,11 +14,12 @@ app.get("/", (req, res) => {
 app.get("/api/products", (req, res) => {
   res.json(products);
 });
+
 app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id == req.params.id);
+  const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
 
-app.listen(PORT, (req, res) => {
-  console.log(`Server listening on ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
 });
