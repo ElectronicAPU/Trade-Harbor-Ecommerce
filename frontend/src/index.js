f867e3d72bdab6bc173870153ab47decd9c44731
ElectronicAPU <apurbanaskar3206@gmail.com>
2024-01-11 17:48:16 +0530

    Implement the REDUX and REDUX Toolkit and added two file called store and apiSlice and also intrigate the Provider in index.js file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,9 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Provider } from "react-redux";
+import store from "./store";
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,8 +30,10 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <ToastContainer />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </Provider>
   </React.StrictMode>
 );
 
