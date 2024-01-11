2038e3c90c44f2b60680cc2938c471a193b8ca62
ElectronicAPU <apurbanaskar3206@gmail.com>
2024-01-11 16:52:54 +0530

    Handle the ERROR in middleware folder

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -2,24 +2,16 @@ import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productsModel.js";
 
 export const getAllProducts = asyncHandler(async (req, res) => {
-  try {
-    const products = await Product.find({});
-    return res.json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  const products = await Product.find({});
+  return res.json(products);
 });
 
 export const singelProduct = asyncHandler(async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      return res.json(product); // Use return here to prevent further code execution
-    }
-
-    return res.status(404).json({ message: "Product not found" }); // Use return here as well
-  } catch (error) {
-    console.error(error.message);
-    return res.status(500).json({ message: "Internal Server Error" });
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
   }
 });
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { errorHanler, notFound } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
 
@@ -14,6 +15,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHanler);
 
 app.listen(port, () => {
   console.log(`Server listening on ${port}`.black.bgWhite);
