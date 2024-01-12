4ce5683d1f3e311d69314a20f333625887ebc907
ElectronicAPU <apurbanaskar3206@gmail.com>
2024-01-13 00:47:27 +0530

    AddToCart funcanility and state menagement in redux

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,19 +1,37 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
-import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  Image,
+  ListGroup,
+  Row,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import { addToCart } from "../slices/cartSlice";
+import { useDispatch } from "react-redux";
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
+  const [qty, setQty] = useState(1);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const {
     data: product,
     isLoading,
     error,
   } = useGetProductDetailsQuery(productId);
+
+  // console.log(dispatch(addToCart({...product, qty})));
+  const addToCartHandler = () => {
+    dispatch(addToCart({...product, qty}));
+  };
 
   return (
     <>
@@ -69,11 +87,32 @@ const ProductScreen = () => {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty:</Col>
+                      <Col>
+                        <Form.Control
+                          as="select"
+                          value={qty}
+                          onChange={(e) => setQty(Number(e.target.value))}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
                 <ListGroup.Item>
                   <Button
                     className="btn-block"
                     type="button"
                     disabled={product.countInStock === 0}
+                    onClick={addToCartHandler}
                   >
                     Add To Cart
                   </Button>
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -2,7 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
-  : { cartItems: [] };
+  : {
+      cartItems: [],
+      itemsPrice: 0,
+      shippingPrice: 0,
+      taxPrice: 0,
+      totalPrice: 0,
+    };
 
 const addDecimals = (nums) => {
   return (Math.round(nums * 100) / 100).toFixed(2);
@@ -14,34 +20,38 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: (state, action) => {
       const item = action.payload;
-      const existItem = state.cartItems.find((x) => x._id === item._id);
+      const existItem = state.cartItems.find((x) => x._id == item._id);
 
       if (existItem) {
         state.cartItems = state.cartItems.map((x) =>
-          x._id === existItem._id ? item : x
+          x._id == existItem._id ? item : x
         );
       } else {
         state.cartItems = [...state.cartItems, item];
       }
-      //Calculate Item Price
+
+      // Calculate Item Price
       state.itemsPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+        state.cartItems.reduce(
+          (acc, item) => acc >= 0 && acc + item.price * item.qty,
+          0
+        )
       );
 
-      //Calculate Shipping Price (if order is over $100 then free, else $10 shipping)
+      // Calculate Shipping Price (if order is over $100 then free, else $10 shipping)
       state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
 
-      //Calculate Tax Price
-      state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice).toFixed(2));
+      // Calculate Tax Price
+      state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice));
 
-      //Calculate Total Price
+      // Calculate Total Price
       state.totalPrice =
         Number(state.itemsPrice) +
         Number(state.shippingPrice) +
         Number(state.taxPrice).toFixed(2);
 
-      // Set all the State to localstorage
-      localStorage.setItem("cart", JSON.stringify(state));
+      // Update local storage with a new object to avoid mutations
+      localStorage.setItem("cart", JSON.stringify({ ...state }));
     },
   },
 });
