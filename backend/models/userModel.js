822a47740dfa85af74897a25f6e1a5ae3ada81a7
ElectronicAPU <apurbanaskar3206@gmail.com>
2024-01-15 16:29:56 +0530

    Register a user and hash the password before register a user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -13,9 +13,19 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+//For Login
 userSchema.methods.matchPassword = async function (enterPassword) {
-  return await bcrypt.compare(enterPassword, this.password)
- }
+  return await bcrypt.compare(enterPassword, this.password);
+};
+
+//For Register users
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
