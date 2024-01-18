06e1b7305a5f38f43d5cd16962e6eba7afcdce28
ElectronicAPU <apurbanaskar3206@gmail.com>
2024-01-18 13:09:30 +0530

    Edit product list in frontend and backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,6 +3,7 @@ import {
   createProduct,
   getAllProducts,
   singelProduct,
+  updateProduct,
 } from "../controller/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
@@ -10,6 +11,6 @@ const router = express.Router();
 
 // Products Routes
 router.route("/").get(getAllProducts).post(protect, admin, createProduct);
-router.route("/:id").get(singelProduct);
+router.route("/:id").get(singelProduct).put(protect, admin, updateProduct);
 
 export default router;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -27,6 +27,7 @@ import ProfileScreen from "./screens/ProfileScreen";
 import AdminRoute from "./components/AdminRoute";
 import OrderListScreen from "./screens/admin/OrderListScreen";
 import ProductListScreen from "./screens/admin/ProductListScreen";
+import ProductEditScrren from "./screens/admin/ProductEditScrren";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -48,6 +49,7 @@ const router = createBrowserRouter(
       <Route path="" element={<AdminRoute />}>
         <Route path="/admin/orderlist" element={<OrderListScreen />} />
         <Route path="/admin/productlist" element={<ProductListScreen />} />
+        <Route path="/admin/product/:id/edit" element={<ProductEditScrren />} />
       </Route>
     </Route>
   )
